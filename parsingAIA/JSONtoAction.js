2da3c492e254398f683af6193929cb50d3cdbc4d
Tom Scharstein <trscharstein@gmail.com>
2021-12-27 00:00:18 -0700

    Either the best code I've ever written or the most needlessly obscure

--- parsingAIA/JSONtoAction.js
+++ parsingAIA/JSONtoAction.js
@@ -1,3 +1,8 @@
+/**
+ * I'm hoping this part will be much easier. I'll probably need to have a lookup function for key/value,
+ * then determine the formatting and encoding while recursing down and using depth to determine tabs
+ */
+
 class JSONtoAction {
   raw = null;
   data = null;
@@ -22,10 +27,72 @@ class JSONtoAction {
     return this.data;
   }
   createSchema() {
-    let temp = this.raw;
-    console.log("Hello world!");
+    let temp = unfoldPerDepth(this.raw);
+    this.value = temp;
+    console.log(temp);
   }
 }
+
+function unfoldPerDepth(obj, str = "", depth = 0, parent = null, enumI = 0) {
+  let padding = "";
+  for (let i = 1; i <= depth; i++) padding += "\\t";
+  Object.keys(obj).forEach((key) => {
+    let conditions = hasConditionalFormatting(key, obj[key], depth, obj);
+    if (conditions.isNone) {
+      str += `${padding}/${key} ${obj[key]}`;
+    } else if (conditions.isHex) {
+      let hex = asciiToHex(obj[key]);
+      str += `${padding}/${key} [ ${
+        hex.length / 2
+      }\r\n${padding}\t${hex}\r\n${padding}]`;
+    } else if (conditions.isDec) {
+      str += `${padding}/${key} ${asciiToDecimal(obj[key])}`;
+    } else if (conditions.isEnum) {
+      str += "bop";
+    }
+    str += "\r\n";
+  });
+  return str;
+}
+
+/**
+ * Is this extremely clever or extremely naive? I can't tell. I've never created validation engines before,
+ * this is probably similar to how custom input elements run dynamic validation rules?
+ *
+ * The idea is that I have an unspecified and possibly dynamic number of edge cases. If I can feed all the data
+ * into an array containing functions that specifically check for some particular edge case, I can filter that.
+ *
+ * If I can guarantee each particular edge case from some particular function, I know what formatting is needed.
+ */
+function hasConditionalFormatting(key, value, depth, parent) {
+  let conditions = {
+    isEnum: [() => ["actions", "events", "parameters"].includes(key)],
+    isHex: [() => depth == 0 && ["name"].includes(key)],
+    isDec: [() => depth > 2 && ["key"].includes(key)],
+  };
+  let diagnostic = Object.keys(conditions)
+    .map((k) => {
+      return [
+        ...new Set(
+          conditions[k]
+            .map((func) => {
+              return func(...arguments) ? k : false;
+            })
+            .filter((i) => i && i.length)
+        ),
+      ];
+    })
+    .flat()
+    .filter((i) => i);
+  return {
+    isEnum: diagnostic.includes("isEnum"),
+    isHex: diagnostic.includes("isHex"),
+    isDec: diagnostic.includes("isDec"),
+    isNone: !diagnostic.length,
+  };
+}
+
+function lookup(key, value, depth, parent) {}
 
 function isJSON(str) {
   try {
