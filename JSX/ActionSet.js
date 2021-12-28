4fffaeb2efd1afcd1035e62f6cbaa2f2a42b7ed8
Tom Scharstein <trscharstein@gmail.com>
2021-12-27 23:31:02 -0700

    JSX version of AIA > JSON passing

--- JSX/ActionSet.js
+++ JSX/ActionSet.js
@@ -82,7 +82,163 @@ function ActionBoyConvertAIAToJSON(rawtext) {
   for (var cc = 0; cc < rootProps.length; cc++)
     chain.push(ActionBoyRecurseForSchema(rootProps[cc], data));
 
-  return data;
+  // Sanitize schema here
+  var sanitizedSchema = ActionBoySanitizeSchema(chain);
+
+  // Return translation:
+  return ActionBoyTranslateSchema(sanitizedSchema);
+}
+
+function ActionBoyTranslateSchema(data, depth) {
+  depth = depth || 0;
+  var result = {};
+  if (data && data.length && ActionBoyIsArray(data)) {
+    for (var entry of data) {
+      var isEnum = /^\/([^\s-]*)-\d{1,}/;
+      if (isEnum.test(ActionBoyTrim(entry.raw))) {
+        if (entry && entry.name) {
+          var enumName = entry.name.replace(/-.*/, "") + "s";
+          result[enumName] = result[enumName] || [];
+          if (entry.children && entry.children.length)
+            result[enumName].push(
+              ActionBoyTranslateSchema(entry.children, depth + 1)
+            );
+        } else {
+          console.log("Something isn't iterating correctly:");
+          result["errorFlags"] = result["errorFlags"] || [];
+          result.errorFlags.push(entry);
+        }
+      } else {
+        // Otherwise we can simply pass in the value and type to know how to handle each property:
+        result[entry.name] = ActionBoySanitizeValue(entry.value, entry.type);
+      }
+    }
+  }
+  return result;
+}
+
+// This should be simple if we've correctly determined the type above
+function ActionBoySanitizeValue(value, type) {
+  var temp = ActionBoyTrim(value);
+  if (/hex/i.test(type)) {
+    temp = ActionBoyTrim(value.replace(/(\[|\])/gm, ""));
+    temp = ActionBoyFilter(temp.split(/\t{1,}/gm), function (i, ii) {
+      return i.length && ii && /^[a-f0-9]*$/.test(i);
+    }).join("");
+    temp = ActionBoyHexToAscii(temp);
+  } else if (/decimal/i.test(type)) {
+    temp = ActionBoyDecimalToAscii(value);
+  } else if (!isNaN(Number(value))) {
+    // This is probably an integer of some kind. Just in case it fits a decimal format:
+    if (/^\d{10}$/.test(temp)) {
+      temp = ActionBoyDecimalToAscii(temp);
+    } else temp = +value; // Otherwise, let's just convert it to a number
+  } else {
+    // Chances are this is just a normal string, no need to do anything special
+    temp = value;
+  }
+  return temp;
+}
+
+// This is the only part of the process I'm very iffy on. It should be better than this.
+//
+// "Sanitization" in this context means taking a closer look at each line entry to determine
+// if it will need decoding and to define the entry/key:value type. Realistically I should be
+// sanitizing and translating at the same time.
+function ActionBoySanitizeSchema(data) {
+  var temp = [];
+  ActionBoyForEach(data, function (rootPropGroup) {
+    var rootClone = {
+      depth: rootPropGroup.depth,
+      raw: rootPropGroup.raw,
+      index: rootPropGroup.index,
+    };
+    // Diagnose and handle any propGroup which may need recursion:
+    if (rootPropGroup["children"] && rootPropGroup["children"].length) {
+      var isDeep = ActionBoyFilter(rootPropGroup["children"], function (i) {
+        return i.children && i.children.length;
+      }).length;
+
+      if (!isDeep) {
+        rootClone["value"] =
+          rootClone.raw +
+          ActionBoyMap(rootPropGroup.children, function (i) {
+            return i.raw;
+          }).join("");
+
+        // Damn. Could I make uglier code if I tried?
+        var str = ActionBoyTrim(rootClone.value);
+        rootClone["name"] = /\/([^\s]*)/.exec(str)[1];
+        str = ActionBoyTrim(str.replace(/^\/([^\s]*)/, ""));
+        rootClone.value = str;
+
+        if (/\{/.test(rootClone.value) && /\}/.test(rootClone.value)) {
+          rootClone["type"] = "container-B";
+          str = ActionBoyTrim(str.replace(/^(\{)|(\})$/gm, ""));
+
+          rootClone.value = str;
+          rootClone["children"] = ActionBoyMap(
+            str.split(/\t{1,}/gm),
+            function (subProp) {
+              var subClone = {
+                index: -1,
+                depth: rootClone.depth + 1,
+                name: ActionBoyTrim(subProp)
+                  .replace(/^\//, "")
+                  .replace(/\s.*/, ""),
+                raw: subProp,
+                value: ActionBoyTrim(
+                  ActionBoyTrim(subProp)
+                    .replace(/^\//, "")
+                    .replace(/^[\s]*/, "")
+                ),
+              };
+              // This here is sloppy
+              subClone.value = new RegExp(`^${subClone.name}`).test(
+                subClone.value
+              )
+                ? ActionBoyTrim(
+                    subClone.value.replace(new RegExp(`^${subClone.name}`), "")
+                  )
+                : subClone.value;
+              if (subClone.value + "" == "null")
+                subClone.value = ActionBoyTrim(
+                  ActionBoyTrim(subProp).replace(/^\/[^\s]/, "")
+                );
+
+              return subClone;
+            }
+          );
+        } else if (/\[/.test(rootClone.value) && /\]/.test(rootClone.value))
+          rootClone["type"] = "hexadecimal";
+        else rootClone["type"] = "undefined-A";
+      } else {
+        rootClone["name"] = ActionBoyTrim(rootClone.raw).replace(/^\//, "");
+        rootClone.name = rootClone.name.match(/[^-]*-\d{1,}/)[0];
+        rootClone["type"] = "container-A";
+        rootClone["children"] = ActionBoySanitizeSchema(rootPropGroup.children);
+      }
+      temp.push(rootClone);
+    } else if (/^\t*\/[^\s]*/.test(rootPropGroup.raw)) {
+      // This is probably a one-line value:
+      rootClone["type"] = "param";
+      rootClone["value"] = ActionBoyTrim(rootPropGroup.raw);
+      if (/\/[^\s]*/.test(rootClone.value))
+        rootClone["name"] = /\/([^\s]*)/.exec(rootClone.value)[1];
+      else rootClone["name"] = "undefined-B";
+      rootClone.value = ActionBoyTrim(
+        rootClone.value.replace(`\/${rootClone.name}`, "")
+      );
+
+      if (/^\d{10}$/.test(rootClone.value)) rootClone["type"] = "decimal";
+
+      temp.push(rootClone);
+    } else {
+      // This is almost certainly a closing bracket, empty newline, or format artifact with no real value.
+      // We don't need this and can ignore it.
+    }
+  });
+  return temp;
 }
 
 // Wow, why didn't I do this before? That's way better than the Node approach
@@ -110,8 +266,11 @@ function ActionBoyIsJSON(str) {
 }
 /**
  * If I'm not mistaken, the only code I use in actionToJSON that would need polyfilling are
- * Array methods: map, filter, isArray, flat, forEach; then JSON and String.padStart.
+ * Array methods: map, filter, isArray, flat, forEach; then JSON, String.padStart and String.trim?
  */
+function ActionBoyTrim(str) {
+  return str.replace(/^\s*|\s*$/gm, "");
+}
 function ActionBoyIsArray(arg) {
   return Object.prototype.toString.call(arg) === "[object Array]";
 }
@@ -134,7 +293,6 @@ function ActionBoyFilter(array, callback, debug) {
 function ActionBoyForEach(array, callback) {
   for (var i = 0; i < array.length; i++) callback(array[i], i, array);
 }
-
 function ActionBoyHexToAscii(input) {
   var output = "";
   for (var i = 0; i < input.toString().length; i += 2)
@@ -149,11 +307,10 @@ function ActionBoyAsciiToHex(input) {
   return output;
 }
 function ActionBoyAsciiToDecimal(input) {
-  return parseInt(asciiToHex(input), 16);
+  return parseInt(ActionBoyAsciiToHex(input), 16);
 }
-
 function ActionBoyDecimalToAscii(input) {
-  return hexToAscii(Number((input + "").trim()).toString(16));
+  return ActionBoyHexToAscii(Number((input + "").trim()).toString(16));
 }
 
 function test() {
